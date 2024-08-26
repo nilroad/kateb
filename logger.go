a52d5a825c4126d75e15db62e9dd30ca305369a0
sina <sinahatami1379@gmail.com>
2024-08-26 18:18:31 +0330

    feat: colories log for stdout

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package kateb
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
@@ -8,6 +9,26 @@ import (
 )
 
 var logger Logger
+
+// ANSI color codes for terminal output
+const (
+	colorReset         = "\033[0m"
+	colorBlack         = "\033[30m"
+	colorRed           = "\033[31m"
+	colorGreen         = "\033[32m"
+	colorYellow        = "\033[33m"
+	colorBlue          = "\033[34m"
+	colorMagenta       = "\033[35m"
+	colorCyan          = "\033[36m"
+	colorWhite         = "\033[37m"
+	colorBrightRed     = "\033[91m"
+	colorBrightGreen   = "\033[92m"
+	colorBrightYellow  = "\033[93m"
+	colorBrightBlue    = "\033[94m"
+	colorBrightMagenta = "\033[95m"
+	colorBrightCyan    = "\033[96m"
+	colorBrightWhite   = "\033[97m"
+)
 
 func init() {
 	logger.sl = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
@@ -46,23 +67,28 @@ type Config struct {
 }
 
 type Logger struct {
-	sl     *slog.Logger
-	config Config
+	sl             *slog.Logger
+	config         Config
+	shouldColorize bool
 }
 
 func New(writer io.Writer, config Config) *Logger {
+	shouldColorize := writer == os.Stdout
+
 	return &Logger{
 		sl: slog.New(slog.NewJSONHandler(writer, &slog.HandlerOptions{
 			AddSource: config.AddSource,
 			Level:     config.Level,
 			ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
 				if a.Key == "msg" {
+
 					return slog.Attr{
 						Key:   "message",
 						Value: a.Value,
 					}
 				}
 				if a.Key == "time" {
+
 					return slog.Attr{
 						Key:   "time",
 						Value: slog.StringValue(a.Value.Time().Format(time.RFC3339)),
@@ -72,28 +98,56 @@ func New(writer io.Writer, config Config) *Logger {
 				return a
 			},
 		})),
-		config: config,
+		config:         config,
+		shouldColorize: shouldColorize,
 	}
 }
+func (r *Logger) setColor(level slog.Level) *Logger {
+	switch level {
+	case slog.LevelDebug:
+		fmt.Println(colorCyan)
+	case slog.LevelInfo:
+		fmt.Println(colorWhite)
+	case slog.LevelWarn:
+		fmt.Println(colorBrightYellow)
+	case slog.LevelError:
+		fmt.Println(colorRed)
+	default:
+		fmt.Println(colorReset)
+	}
 
+	return r
+}
+func (r *Logger) restColor() *Logger {
+	fmt.Println(colorReset)
+
+	return r
+}
 func (r *Logger) Error(message string, args map[string]any) {
-	r.sl.Error(r.config.Prefix+":"+message, contextKey, args)
+
+	r.setColor(slog.LevelError).sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 }
 func (r *Logger) Fatal(message string, args map[string]any) {
-	r.sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	r.setColor(slog.LevelError).sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 	os.Exit(1)
 }
 func (r *Logger) Info(message string, args map[string]any) {
-	r.sl.Info(r.config.Prefix+":"+message, contextKey, args)
+	r.setColor(slog.LevelInfo).sl.Info(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 }
 func (r *Logger) Debug(message string, args map[string]any) {
-	r.sl.Debug(r.config.Prefix+":"+message, contextKey, args)
+	r.setColor(slog.LevelDebug).sl.Debug(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 }
 func (r *Logger) Warn(message string, args map[string]any) {
-	r.sl.Warn(r.config.Prefix+":"+message, contextKey, args)
+	r.setColor(slog.LevelWarn).sl.Warn(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 }
 func (r *Logger) Panic(message string, args map[string]any) {
-	r.sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	r.setColor(slog.LevelError).sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	r.restColor()
 	panic(message)
 }
 
