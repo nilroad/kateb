4b3c43f5f4b6c27abef4789a66abe87a1e9d3e8f
DOO-DEV <nimo.vimo.yank@gmail.com>
2024-08-26 16:59:11 +0330

    feat: add default logger

--- logger.go
+++ logger.go
@@ -7,6 +7,36 @@ import (
 	"time"
 )
 
+var logger Logger
+
+func init() {
+	logger.sl = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+			if a.Key == "msg" {
+				return slog.Attr{
+					Key:   "message",
+					Value: a.Value,
+				}
+			}
+			if a.Key == "time" {
+				return slog.Attr{
+					Key:   "time",
+					Value: slog.StringValue(a.Value.Time().Format(time.RFC3339)),
+				}
+			}
+
+			return a
+		},
+	}))
+}
+
+// Set you can change the default logger
+func Set(l Logger) {
+	logger = l
+}
+
 const contextKey = "context"
 
 type Config struct {
@@ -64,5 +94,26 @@ func (r *Logger) Warn(message string, args map[string]any) {
 }
 func (r *Logger) Panic(message string, args map[string]any) {
 	r.sl.Error(r.config.Prefix+":"+message, contextKey, args)
+	panic(message)
+}
+
+func Error(message string, args map[string]any) {
+	logger.sl.Error(message, contextKey, args)
+}
+func Fatal(message string, args map[string]any) {
+	logger.sl.Error(message, contextKey, args)
+	os.Exit(1)
+}
+func Info(message string, args map[string]any) {
+	logger.sl.Info(message, contextKey, args)
+}
+func Debug(message string, args map[string]any) {
+	logger.sl.Debug(message, contextKey, args)
+}
+func Warn(message string, args map[string]any) {
+	logger.sl.Warn(message, contextKey, args)
+}
+func Panic(message string, args map[string]any) {
+	logger.sl.Error(message, contextKey, args)
 	panic(message)
 }
